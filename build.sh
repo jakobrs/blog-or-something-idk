2eec35a5ef5fe2cc146fdb3ca2c45d9d7b1a6256
Jakob Skaar <jakobrs100@gmail.com>
2026-08-08 19:53:18 +0200

    Make build simpler

--- build.sh
+++ build.sh
@@ -1,25 +1,3 @@
 #!/usr/bin/env bash
-
-format() {
-    case "$1" in
-        html) echo html;;
-        mathml) echo html;;
-        pdf) echo pdf;;
-    esac
-}
-ending() {
-    case "$1" in
-        html) echo .html;;
-        mathml) echo .mathml.html;;
-        pdf) echo .pdf;;
-    esac
-}
-
-compile() {
-    for fmt in {html,mathml,pdf}; do
-        typst compile --root "$PWD" --features html --format "$(format $fmt)" "posts/$1.typ" "docs/$1$(ending $fmt)" --input "format=$fmt"
-    done
-}
-
 rm docs/*
-typst compile --root "$PWD" --features bundle,html --format bundle posts/root.typ docs
+typst compile --root "$PWD" --features bundle,html --format bundle posts/root.typ docs --creation-timestamp 0
